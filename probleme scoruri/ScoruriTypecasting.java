4916e93e969b3a3ed93125c4a00cc718e00bf0b3
ArlynAnnelie <120953583+ArlynAnnelie@users.noreply.github.com>
2023-01-07 21:06:18 +0200

    Create ScoruriTypecasting.java

---
+++ probleme scoruri/ScoruriTypecasting.java
@@ -0,0 +1,11 @@
+//Media a trei scoruri(note) cu convertire (Type casting) din int în float
+
+public class ScoruriTypeCasting {
+	public static void main(String[] args) {
+		int score1 = 72;
+		int score2 = 73;
+		int score3 = 33;
+		
+		System.out.print("Media: " + (score1 + score2 + score3) / (float) 3);
+	}
+}
