121d33b7b897c83e9265137026eb949b55a679ae
ArlynAnnelie <120953583+ArlynAnnelie@users.noreply.github.com>
2023-01-08 15:44:32 +0200

    Create ScoruriCuFunctiaMedie.java

---
+++ probleme scoruri/ScoruriCuFunctiaMedie.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ScoruriCuFunctiaMedie {
+	static int n; // variabilă globală
+	static int[] scoruri; // vector alocat global(în membrul clasei)
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("n = ");
+		n = sc.nextInt();
+		scoruri = new int[n];
+		System.out.println("Scrie elementele vectorului: ");
+		for (int i = 0; i < n; i++) {
+			scoruri[i] = sc.nextInt();
+		}
+		medie(scoruri);
+		System.out.print("Media scorurilor este: " + medie(scoruri));
+		sc.close();
+	}
+
+	public static float medie(int vector[]) {
+		int suma = 0; // variabila care numară
+		for (int i = 0; i < n; i++) {
+			suma += vector[i]; // sau: suma =
+		}
+		return suma / (float) n;
+	}
+}
