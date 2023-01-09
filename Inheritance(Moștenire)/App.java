0c3e621bd84562de7c591f8bc2861a526d3283f8
ArlynAnnelie <120953583+ArlynAnnelie@users.noreply.github.com>
2023-01-09 21:56:23 +0200

    Create App.java

---
+++ Inheritance(Moștenire)/App.java
@@ -0,0 +1,21 @@
+//Inheritance(moștenire) e atunci când o clasă în Java "moștenește" alte metode dintr-o altă clasă din Java
+
+public class App {
+
+	public static void main(String[] args) {
+		Cântăret SelenaGomez = new Cântăret(); //crearea unui simplu obiect Cântăret
+		SelenaGomez.cântăPop();
+		
+		
+		Shakira cântăreață = new Shakira(); //crearea unui obiect de la clasa Shakira
+		cântăreață.cântăPop();
+		
+		Elvis cântăreț = new Elvis(); //crearea unui obiect de la clasa Elvis
+		cântăreț.cântăBlues();
+
+	}
+
+}
+
+//cu inheritance(moștenire) puteți face fiecare dintre cei 2 cântăreți, Shakira și Elvis 
+//să cânte orice gen de muzică doriți voi.
