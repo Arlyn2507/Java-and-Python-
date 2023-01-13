43637ab97c6015a66b2f23c1efbf87f302a4ea25
ArlynAnnelie <120953583+ArlynAnnelie@users.noreply.github.com>
2023-01-13 12:56:06 +0200

    Create StructuraIfSimplă.java

---
+++ Structura if/StructuraIfSimplă.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class StructuraIfSimplă {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//Scrie o valoare pentru x:  
+		System.out.print("x = ");
+		int x = sc.nextInt();
+
+		//Scrie o valoare pentru y:  
+		System.out.print("y = ");
+		int y = sc.nextInt();
+
+		if (x < y) {
+			System.out.println();
+			System.out.println("x este mai mic decât y");
+		}
+		sc.close();
+	}
+
+}
+
+// input:
+// x = 23
+// y = 43
+
+// output: x este mai mic decât y
