dd2e91d98621d6333fad790194a400b63c5f8e80
ArlynAnnelie <120953583+ArlynAnnelie@users.noreply.github.com>
2023-01-14 15:42:17 +0200

    Create DivizoriiNaturaliAiUnuiNumăr.java

---
+++ probleme de numărare/DivizoriiNaturaliAiUnuiNumăr.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class DivizoriiNaturaliAiUnuiNumăr {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// Scrie o valoare pentru n:
+		System.out.print("n = ");
+		int n = sc.nextInt();
+
+		System.out.println("Divizorii numărului " + n + " sunt: ");
+
+		for (int d = 1; d <= n / 2; d++) { // se parcurg toate numerele din intervalul de la 1 la n / 2
+			if (n % d == 0) { // se selectează acele numere d care sunt sunt divizori ai numărului n
+				System.out.print(d + " ");
+			}
+			sc.close();
+		}
+	}
+}
+
+// n % d == 0 => x se împarte exact la d, împărțire care dă restul 0
