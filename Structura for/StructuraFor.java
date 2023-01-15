c99aa646c944d0457b1de7b529f24c0a43e6e110
ArlynAnnelie <120953583+ArlynAnnelie@users.noreply.github.com>
2023-01-15 11:03:32 +0200

    Create StructuraFor.java

---
+++ Structura for/StructuraFor.java
@@ -0,0 +1,18 @@
+public class StructuraFor {
+
+	public static void main(String[] args) {
+        
+		System.out.println("Pisica mea face meow de 3 ori: ");
+		for (int i = 0; i < 3; i++) { 
+			System.out.println("meow");
+		}
+	}
+
+}
+
+// stuctura for initializeaza variabila i, apoi verifica daca acea conditie (i < 3) este adevărată, 
+// dacă este adevărată va executa instructiunea din acolade, apoi va incrementa i-ul cu o unitate
+// când i-ul a depășit valoarea 3 se va ieși din structură
+
+
+// cu structura for se parcurg vectori, matrici și intervale
