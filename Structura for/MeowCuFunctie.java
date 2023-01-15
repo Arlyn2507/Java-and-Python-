75c1882be7cc8c2d0fd4e9683c4fde30317fadd8
ArlynAnnelie <120953583+ArlynAnnelie@users.noreply.github.com>
2023-01-15 11:13:15 +0200

    Create  MeowCuFunctie.java

---
+++ Structura for/MeowCuFunctie.java
@@ -0,0 +1,13 @@
+public class MeowCuFunctie {
+
+	public static void main(String[] args) {
+	
+		for (int i = 0; i < 3; i++) {
+			meow();
+		}
+	}
+	
+	public static void meow () {
+		System.out.println("meow");
+	}
+}
